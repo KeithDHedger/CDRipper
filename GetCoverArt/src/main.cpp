8bae4ac483e0b01c2dba8a7f1821bdcebf1fa972
keithhedger <keithhedger@59be48dd-a9fb-489d-a65a-3046c20450e5>
2013-05-04 21:47:29 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/CDRipper@2 59be48dd-a9fb-489d-a65a-3046c20450e5

--- GetCoverArt/src/main.cpp
+++ GetCoverArt/src/main.cpp
@@ -8,13 +8,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <glib.h>
 
 #define VERSION "0.0.0"
 #define UNKNOWNARG -100
 
 struct option long_options[] =
 	{
-		{"long1",1,0,'l'},
+		{"artist",1,0,'a'},
+		{"album",1,0,'b'},
 		{"version",0,0,'v'},
 		{"help",0,0,'?'},
 		{0, 0, 0, 0}
@@ -31,20 +33,110 @@ printf("Usage: getcoverart [OPTION]\n"
 	);
 }
 
+//string sliceing
+
+char* slice(char* srcstring,int tmpstartchar,int tmpendchar)
+{
+	char*	dest;
+	int		strsize;
+	int		startchar=tmpstartchar;
+	int		endchar=tmpendchar;
+
+	if(tmpstartchar<0)
+		startchar=0;
+
+	if((tmpendchar<0) || (tmpendchar>(int)strlen(srcstring)))
+		endchar=strlen(srcstring)-1;
+
+	strsize=endchar-startchar+1;
+
+	dest=(char*)malloc(strsize+1);
+	strncpy(dest,(char*)&srcstring[startchar],strsize);
+	dest[strsize]=0;
+
+	return(dest);
+}
+
+char* sliceBetween(char* srcstring,char* startstr,char* endstr)
+{
+	int		startchar;
+	int		endchar;
+	char*	ptr;
+	char*	dest=NULL;
+
+	ptr=strstr(srcstring,startstr);
+	if(ptr==NULL)
+		return(NULL);
+	startchar=(int)(long)ptr+strlen(startstr)-(long)srcstring;
+
+	ptr=strstr((char*)&srcstring[startchar],endstr);
+	if(ptr==NULL)
+		return(NULL);
+	endchar=(int)(long)ptr-(long)srcstring-1;
+
+	dest=slice(srcstring,startchar,endchar);
+	return(dest);
+}
+
+char* sliceLen(char* srcstring,int tmpstartchar,int len)
+{
+	char*	dest;
+	int		strsize;
+	int		startchar=tmpstartchar;
+	int		endchar=len;
+
+	if(tmpstartchar<0)
+		startchar=0;
+
+	if((len<0) || (len+startchar>(int)strlen(srcstring)))
+		endchar=strlen(srcstring)-startchar;
+
+	strsize=endchar;
+
+	dest=(char*)malloc(strsize+1);
+	strncpy(dest,(char*)&srcstring[startchar],endchar);
+	dest[endchar]=0;
+
+	return(dest);
+
+}
+
+char* sliceStrLen(char* srcstring,char* startstr,int len)
+{
+	char*	ptr;
+	int		startchar;
+
+	ptr=strstr(srcstring,startstr);
+	if(ptr==NULL)
+		return(NULL);
+	startchar=(int)(long)ptr+strlen(startstr)-(long)srcstring;
+	printf("%i\n",startchar);
+	return(sliceLen(srcstring,startchar,len));
+}
+
+char*	artist;
+char*	album;
+
 int main(int argc, char **argv)
 {
 	int c;
 	while (1)
 		{
 		int option_index = 0;
-		c = getopt_long (argc, argv, "v?h:l:",long_options, &option_index);
+		c = getopt_long (argc, argv, "v?h:a:b:",long_options, &option_index);
 		if (c == -1)
 			break;
 
 		switch (c)
 			{
-			case 'l':
+			case 'a':
 				printf("Arg=%s\n",optarg);
+				artist=optarg;
+				break;
+		
+			case 'b':
+				printf("Arg=%s\n",optarg);
+				album=optarg;
 				break;
 		
 			case 'v':
@@ -73,7 +165,27 @@ int main(int argc, char **argv)
 		printf("\n");
 		}
 	
-	printf("%s\n","Hello World");
+	//printf("%s\n","Hello World");
+	char*	url;
+	char	buffer[16384];
+	char*	command;
+	FILE*		fp;
+	album=g_strdelimit(album," ",'+');
+	artist=g_strdelimit(artist," ",'+');
+//	char		line[1024];
+	asprintf(&command,"curl -sk \"https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s+%s&as_filetype=jpg&imgsz=large&rsz=1\"",artist,album);
+
+	fp=popen(command, "r");
+	fgets((char*)&buffer[0],16384,fp);
+//	printf("%s\n",buffer);
+	url=sliceBetween((char*)buffer,"unescapedUrl\":\"","\",\"");
+	printf("%s\n",url);
+	asprintf(&command,"curl -sko folder.jpg %s",url);
+	system(command);
+	
+	pclose(fp);
+	g_free(command);
+	g_free(url);
 	return 0;
 }
 
